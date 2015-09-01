9c5aae5defea0bfb325a988c1d137f4d599e40af
irina petrovskaya <petrovskaya@gmail.com>
2015-09-01 17:40:14 +0300

    Initial commit

---
+++ src/main/java/org/test/fmtest/FreemarkerTestApplication.java
@@ -0,0 +1,12 @@
+package org.test.fmtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreemarkerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreemarkerTestApplication.class, args);
+    }
+}
---
+++ src/main/java/org/test/fmtest/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.test.fmtest;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FreemarkerTestApplication.class);
+	}
+
+}
